c221dc47d31d2c85c81445f8f6a9d516ddc6398e
EdwinTomy <etomygeorg@miners.utep.edu>
2020-03-04 12:22:05 -0700

    Finished test cases Debugged SortOfSort.java Added comments

--- SortOfSort/src/SortOfSort.java
+++ SortOfSort/src/SortOfSort.java
@@ -1,33 +1,47 @@
 public class SortOfSort {
+
+    //Starter method sortOfSort() for recursion
     public static int[] sortOfSort(int[] arr) {
         return sortHelper(arr, 0, arr.length - 1, 0);
     }
 
+    //Helper recursive method sortHelper() for sortOfSort()
+    //startIndex and endIndex are the ends of the unsorted part of the array
+    //counter keeps track which largest number are we on
     public static int[] sortHelper(int[] arr, int startIndex, int endIndex, int counter) {
+
+        //When endIndex and startIndex pass each other, all elements are sorted, returns array
         if(endIndex - startIndex < 0)
             return arr;
 
+        //Finding index of largest element in the unsorted array
         int maxIndex = maxIndex(arr, startIndex, endIndex);
+
+        //When counter is 0 or 1 mod 4, the largest number is swapped to the right
+        //When counter is 2 or 3 mod 4, the largest number is swapped to the left
+        //Recursive call on new, shorter unsorted array
         if (counter % 4 < 2) {
             swap(arr, endIndex, maxIndex);
             return sortHelper(arr, startIndex, endIndex - 1, counter + 1);
         } else {
             swap(arr, startIndex, maxIndex);
-            return sortHelper(arr, startIndex - 1, endIndex, counter + 1);
+            return sortHelper(arr, startIndex + 1, endIndex, counter + 1);
         }
     }
 
+    //Method maxIndex() to find index of largest element on unsorted array
     public static int maxIndex(int[] arr, int startIndex, int endIndex){
         int maxVal = startIndex;
         for(int i = startIndex; i <= endIndex; i++){
-            if(maxVal < arr[i])
+            if(arr[maxVal] < arr[i])
                 maxVal = i;
         }return maxVal;
     }
 
+    //Method swap() swaps two elements of the array
     public static void swap(int[] arr, int index1, int index2){
         int temp = arr[index1];
         arr[index1] = arr[index2];
-        arr[index2]= arr[index1];
+        arr[index2]= temp;
     }
 }
--- SortOfSort/src/SortOfSortTester.java
+++ SortOfSort/src/SortOfSortTester.java
@@ -4,17 +4,94 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 public class SortOfSortTester {
 
+    //Testing for smallest possible non-empty array: PASSED
     @Test
     public void TestSort1(){
         System.out.println("Test Case #1");
+        int[] arr = new int[] {1};
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+
+        int[] arrExpected = new int[] {1};
+        int[] arrResult = SortOfSort.sortOfSort(arr);
+
+        assertArrayEquals(arrExpected,arrResult);
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+    }
+
+    //Testing for when array.length < 4, since the pattern is made of 4 numbers: PASSED
+    @Test
+    public void TestSort2(){
+        System.out.println("Test Case #2");
+        int[] arr = new int[] {2, 5, 4};
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+
+        int[] arrExpected = new int[] {2, 4, 5};
+        int[] arrResult = SortOfSort.sortOfSort(arr);
+
+        assertArrayEquals(arrExpected,arrResult);
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+    }
+
+    //Testing for when array.length is odd, since the pattern wouldn't end perfectly: PASSED
+    @Test
+    public void TestSort3(){
+        System.out.println("Test Case #3");
+        int[] arr = new int[] {1, 2, 3, 5, 0};
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+
+        int[] arrExpected = new int[] {2, 1, 0, 3, 5};
+        int[] arrResult = SortOfSort.sortOfSort(arr);
+
+        assertArrayEquals(arrExpected,arrResult);
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+    }
+
+    //Testing for negative numbers: PASSED
+    @Test
+    public void TestSort4(){
+        System.out.println("Test Case #4");
         int[] arr = new int[] {1, 4, 5, 3, 0, -2, -4};
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+
         int[] arrExpected = new int[] {3, 1, -4, -2, 0, 4, 5};
         int[] arrResult = SortOfSort.sortOfSort(arr);
 
-        for(int i = 0; i < arrResult.length; i++){
-            System.out
-        }
+        assertArrayEquals(arrExpected,arrResult);
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+    }
+
+    //Testing when there are duplicate values: PASSED
+    @Test
+    public void TestSort5(){
+        System.out.println("Test Case #5");
+        int[] arr = new int[] {2, 3, 4, 5, 5, 1, 1};
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+
+        int[] arrExpected = new int[] {4, 3, 1, 1, 2, 5, 5};
+        int[] arrResult = SortOfSort.sortOfSort(arr);
 
         assertArrayEquals(arrExpected,arrResult);
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
     }
+
 }
